fa9076995655cf66c67a12811332887011e7921b
Norbert <Norbert@Norbert-Norbert>
2020-12-12 08:56:42 +0100

    pierwszy

---
+++ test/src/test/plik.java
@@ -0,0 +1,18 @@
+package test;
+
+
+
+public class plik {
+
+	public static void main(String[] args) {
+		
+     double b=4;
+     System.out.println(b);
+     System.out.println(b);
+     b++;
+     System.out.println(b);
+        
+
+	}
+ }
+
